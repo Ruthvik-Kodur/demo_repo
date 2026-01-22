a212d53ae07ebbc5ad37d699f0e74fec5c4ba587
Ruthvik-Kodur <mylapalli.ruthvikkumar@tessolve.com>
2026-01-22 09:29:40 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,8 +1,8 @@
 from basic import add
 import pytest
 
-def add_three_numbers():
+def test_add_three_numbers():
   assert add(5,10,15)==30
 
-def add_two_numbers():
+def test_add_two_numbers():
   assert add(2,10)==15
